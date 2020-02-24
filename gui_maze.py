e299b62f8d1b3c090ce44d2ea45af6c72e665773
Irmen de Jong <irmen@razorvine.net>
2020-02-24 21:49:33 +0100

    maze size selection

--- gui_maze.py
+++ gui_maze.py
@@ -6,9 +6,14 @@ from mazes.generators import *
 from mazes.maze import Maze, dxdy
 from mazes.solvers import DepthFirstSolver, BreadthFirstSolver
 
+maze_sizes = {
+    "normal": (100, 60, 10),
+    "large": (250, 150, 5)
+}
+
 
 class GuiWindow(tkinter.Tk):
-    def __init__(self, columns: int, rows: int, scale: int = 14) -> None:
+    def __init__(self, columns: int, rows: int, scale: int) -> None:
         super().__init__()
         self.columns = columns
         self.rows = rows
@@ -17,12 +22,18 @@ class GuiWindow(tkinter.Tk):
         self.geometry("+400+200")
         self.canvas = tkinter.Canvas(self, bg='light gray', width=columns * self.scale + 1,
                                      height=rows * self.scale + 1)
+        self.resize_maze("normal")
         bf = tkinter.Frame(self)
+        self.sizevar = tkinter.StringVar(self, value="normal")
+        sr1 = tkinter.Radiobutton(bf, text="Normal maze size", value="normal", variable=self.sizevar, selectcolor="black", command=lambda: self.resize_maze("normal"))
+        sr2 = tkinter.Radiobutton(bf, text="Huge maze size", value="large", variable=self.sizevar, selectcolor="black", command=lambda: self.resize_maze("large"))
         b1 = tkinter.Button(bf, text="Depth First Search", command=self.search_dfs)
         b2 = tkinter.Button(bf, text="Breadth First Search", command=self.search_bfs)
+        sr1.pack(side=tkinter.LEFT)
+        sr2.pack(side=tkinter.LEFT)
         b1.pack(side=tkinter.LEFT)
         b2.pack(side=tkinter.LEFT)
-        bf.pack()
+        bf.pack(anchor=tkinter.W)
         self.canvas.pack(fill=tkinter.BOTH, expand=True, padx=4, pady=4)
         self.drawn_tagged_cells: Set[Tuple[int, int]] = set()
 
@@ -50,19 +61,21 @@ class GuiWindow(tkinter.Tk):
     def erase_path(self, path: str) -> None:
         self.canvas.delete("path")
 
-    def draw_path(self, path: str, color: str = "teal") -> None:
+    def draw_path(self, path: str, color: str = "navy") -> None:
         x, y = 0, 0
-        self.canvas.create_rectangle(4 + x * self.scale, 4 + y * self.scale,
-                                     self.scale - 2 + x * self.scale, self.scale - 2 + y * self.scale,
-                                     fill=color, tags="path")
+        pad = {"normal": 4, "large": 3}[self.sizevar.get()]
+        self.canvas.create_rectangle(x * self.scale + pad, y * self.scale + pad,
+                                     (x + 1) * self.scale - pad + 3, (y + 1) * self.scale - pad + 3,
+                                     fill=color, outline="", tags="path")
         for step in path:
             x += dxdy[step][0]
             y += dxdy[step][1]
-            self.canvas.create_rectangle(4 + x * self.scale, 4 + y * self.scale,
-                                         self.scale - 2 + x * self.scale, self.scale - 2 + y * self.scale,
-                                         fill=color, tags="path")
+            self.canvas.create_rectangle(x * self.scale + pad, y * self.scale + pad,
+                                         (x + 1) * self.scale - pad + 3, (y + 1) * self.scale - pad + 3,
+                                         fill=color, outline="", tags="path")
 
     def draw_tagged_cells(self, maze: Maze) -> None:
+        # create a rainbow color table
         colors = []
         for hue in range(256):
             rf, gf, bf = colorsys.hsv_to_rgb(hue / 256, 1, 0.99999)
@@ -74,16 +87,24 @@ class GuiWindow(tkinter.Tk):
         def color(tag: int) -> str:
             return colors[tag % len(colors)]
 
+        pad = {"normal": 3, "large": 2}[self.sizevar.get()]
+
         for y, row in enumerate(maze.cells):
             for x, cell in enumerate(row):
                 if cell.tag is not None and (x, y) not in self.drawn_tagged_cells:
                     self.drawn_tagged_cells.add((x, y))
-                    self.canvas.create_rectangle(3 + x * self.scale, 3 + y * self.scale,
-                                                 self.scale + x * self.scale, self.scale + y * self.scale,
+                    self.canvas.create_rectangle(x * self.scale + pad, y * self.scale + pad,
+                                                 (x + 1) * self.scale - pad + 2, (y + 1) * self.scale - pad + 2,
                                                  fill=color(cell.tag), outline="", tags="path")
+
+    def resize_maze(self, size: str) -> None:
+        self.columns, self.rows, self.scale = maze_sizes[size]
+        self.canvas.config(width=self.columns * self.scale + 1, height=self.rows * self.scale + 1)
 
     def search_bfs(self):
         self.clear()
+        self.resize_maze(self.sizevar.get())
+        fast_forward = {"normal": 2, "large": 20}[self.sizevar.get()]
 
         def solve_maze(maze: Maze) -> None:
             solutions = BreadthFirstSolver().solve_generator(maze)
@@ -93,7 +114,8 @@ class GuiWindow(tkinter.Tk):
                 nonlocal previous_path
                 more = True
                 try:
-                    previous_path = next(solutions)
+                    for _ in range(fast_forward):
+                        previous_path = next(solutions)
                 except StopIteration:
                     more = False
                 self.draw_tagged_cells(maze)
@@ -108,6 +130,8 @@ class GuiWindow(tkinter.Tk):
 
     def search_dfs(self):
         self.clear()
+        self.resize_maze(self.sizevar.get())
+        fast_forward = {"normal": 10, "large": 100}[self.sizevar.get()]
 
         def solve_maze(maze: Maze) -> None:
             solutions = DepthFirstSolver().solve_generator(maze)
@@ -118,7 +142,7 @@ class GuiWindow(tkinter.Tk):
                 more = True
                 path = ""
                 try:
-                    for _ in range(10):
+                    for _ in range(fast_forward):
                         path = next(solutions)
                 except StopIteration:
                     more = False
@@ -138,11 +162,12 @@ class GuiWindow(tkinter.Tk):
         maze_generator = DepthFirstGenerator(self.columns, self.rows)
         mazes = maze_generator.generate_iterative()
         maze = Maze([[]])
+        fast_forward = {"normal": 5, "large": 250}[self.sizevar.get()]
 
         def generate():
             nonlocal maze
             try:
-                for _ in range(maze_generator.suggested_iteration_size * 5):
+                for _ in range(maze_generator.suggested_iteration_size * fast_forward):
                     maze = next(mazes)
             except StopIteration:
                 self.draw_maze(maze)
@@ -155,5 +180,6 @@ class GuiWindow(tkinter.Tk):
 
 
 if __name__ == "__main__":
-    gui = GuiWindow(100, 60, 10)
+    w, h, scale = maze_sizes["normal"]
+    gui = GuiWindow(w, h, scale)
     gui.mainloop()
